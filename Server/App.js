03b789fd3a39ee281de0140719821add7c77a8bd
kiokoeric <kiokoerick040@gmail.com>
2024-02-15 15:27:54 +0300

    first commit

--- Server/App.js
+++ Server/App.js
@@ -10,7 +10,7 @@ dotenv.config();
 // Middleware
 
 app.use(cors({
-    origin: ["https://cook-io-nu.vercel.app"],
+    origin: ["https://cook-io-mu.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }))
