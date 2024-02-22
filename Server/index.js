f3fe8f8e1119214417581851f9a444ca4fad708f
kiokoeric <kiokoerick040@gmail.com>
2024-02-22 18:26:42 +0300

    Changes to index.js

--- Server/index.js
+++ Server/index.js
@@ -9,17 +9,18 @@ dotenv.config();
 
 // Middleware
 
-app.use(cors({
-    Origin: ["https://cook-io-mu.vercel.app"],
-    methods: ["POST", "GET", "PUT", "DELETE", "Access-Control-Allow-Methods"],
-    credentials: true
-}))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
-    res.header('Access-Control-Allow-Methods : GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
-    res.header('Access-Control-Allow-Headers : Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allowed headers
-    next();
-});
+// app.use(cors({
+//     Origin: ["https://cook-io-mu.vercel.app"],
+//     methods: ["POST", "GET", "PUT", "DELETE", "Access-Control-Allow-Methods"],
+//     credentials: true
+// }))
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+//     res.header('Access-Control-Allow-Methods : GET, POST, PUT, DELETE, OPTIONS'); // Allowed HTTP methods
+//     res.header('Access-Control-Allow-Headers : Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allowed headers
+//     next();
+// });
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
