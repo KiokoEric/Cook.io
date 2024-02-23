41df3fa1e5e85307d118ba375920d159e4133fdd
kiokoeric <kiokoerick040@gmail.com>
2024-02-23 16:06:32 +0300

    Server Side Changes

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const cors = require("cors");
 const dotenv = require('dotenv');
 const app = express();
 
@@ -9,19 +9,19 @@ dotenv.config();
 
 // Middleware
 
-// app.use(cors({
-//     Origin: ["https://cook-io-mu.vercel.app"],
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// }))
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin: *'); 
-//     res.header('Access-Control-Allow-Methods : GET, POST, PUT, DELETE, OPTIONS'); 
-//     res.header('Access-Control-Allow-Headers : Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     next()
-// });
+app.use(cors({
+    Origin: ["https://cook-io-mu.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin: *'); 
+    res.header('Access-Control-Allow-Methods : GET, POST, PUT, DELETE, OPTIONS'); 
+    res.header('Access-Control-Allow-Headers : Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next()
+});
 app.use(express.json())
 app.use(cookieParser())
 
