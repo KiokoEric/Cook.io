6db9fe4538bcbd09ee3e4d7d62fd2cc7413dcc38
kiokoeric <kiokoerick040@gmail.com>
2024-02-23 16:21:06 +0300

    Server Side Changes

--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ app.use(cors({
     credentials: true
 }))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin: *'); 
+    res.header('Access-Control-Allow-Origin: https://cook-io-mu.vercel.app'); 
     res.header('Access-Control-Allow-Methods : GET, POST, PUT, DELETE, OPTIONS'); 
     res.header('Access-Control-Allow-Headers : Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
